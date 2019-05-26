9099395241a7f708ad9f3e310880d4a9fdbf2d5c
Rahel <rachelhd.vo@gmail.com>
2019-05-26 19:47:44 +0200

    vis 'luk' ikon mens burger menu er aktiv

--- script.js
+++ script.js
@@ -5,17 +5,23 @@ function sidenVises() {
 
     //burgermenu
     document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+    //ikoner på header
+    //document.querySelector("#header_container").classList.remove("hidden");
 }
 
 function toggleMenu() {
     console.log("toggleMenu");
-    document.querySelector("#menu").classList.toggle("hidden");
-    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
 
+    //åben burgermenu
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
 
     if (erSkjult == true) {
         document.querySelector("#menuknap").classList.toggle("change");
     } else {
         document.querySelector("#menuknap").classList.toggle("change");
     }
+
 }
